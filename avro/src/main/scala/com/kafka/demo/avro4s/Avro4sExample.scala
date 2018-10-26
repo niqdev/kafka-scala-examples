78e7e3ef79409def7a8c9a0177024e35f8495f12
niqdev <niqdev@gmail.com>
2018-10-26 11:06:51 +0100

    add avro4s schema comparison

--- avro/src/main/scala/com/kafka/demo/avro4s/Avro4sExample.scala
+++ avro/src/main/scala/com/kafka/demo/avro4s/Avro4sExample.scala
@@ -1,10 +1,15 @@
 package com.kafka.demo
 package avro4s
 
-private[avro4s] case class User(name: String,
-                                favoriteNumber: Int,
-                                favoriteColor: String)
+import com.sksamuel.avro4s.{AvroDoc, AvroName, AvroNamespace}
 
-class Avro4sExample {
+@AvroName("User")
+@AvroNamespace("com.kafka.demo")
+@AvroDoc("Avro4s User Schema")
+private[avro4s] case class User(name: String,
+                                @AvroName("favorite_number") favoriteNumber: Option[Int],
+                                @AvroName("favorite_color") favoriteColor: Option[String])
+
+object Avro4sExample {
 
 }
--- avro/src/test/scala/com/kafka/demo/avro4s/Avro4sExampleSpec.scala
+++ avro/src/test/scala/com/kafka/demo/avro4s/Avro4sExampleSpec.scala
@@ -1,14 +1,26 @@
 package com.kafka.demo
 package avro4s
 
+import java.io.File
+
+import com.sksamuel.avro4s.AvroSchema
+import org.apache.avro.Schema
 import org.scalatest.{Matchers, WordSpecLike}
 
 final class Avro4sExampleSpec extends WordSpecLike with Matchers {
 
   "Avro4sExample" should {
 
-    "serialize and deserialize" in {
-      ???
+    "verify schema" in {
+      val schemaPath = "avro/src/main/avro/user.avsc"
+      val originalSchema = new Schema.Parser().parse(new File(schemaPath))
+
+      val schema: Schema = AvroSchema[avro4s.User]
+
+      // "doc" field comparison is ignored
+      println(schema)
+      println(originalSchema)
+      schema shouldBe originalSchema
     }
 
   }
