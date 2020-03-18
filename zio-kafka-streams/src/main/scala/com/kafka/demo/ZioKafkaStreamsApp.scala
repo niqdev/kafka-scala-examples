8856bdf8255d90317818e2b61c8a76f79fcbf787
niqdev <niqdev@users.noreply.github.com>
2020-03-18 17:33:28 +0000

    zio-ks: update main

--- zio-kafka-streams/src/main/scala/com/kafka/demo/ZioKafkaStreamsApp.scala
+++ zio-kafka-streams/src/main/scala/com/kafka/demo/ZioKafkaStreamsApp.scala
@@ -4,28 +4,30 @@ import zio.config.{Config, config}
 import zio.logging.{Logging, log}
 import zio.{App, UIO, ZEnv, ZIO}
 
-// sbt "zio-kafka-streams/runMain com.kafka.demo.ZioKafkaStreamsApp"
 object ZioKafkaStreamsApp extends App {
 
-  private[this] lazy val configLayer = Config.fromMap(Map(
+  private[this] final type AppEnv = Config[KafkaStreamsConfig] with Logging
+
+  private[this] final lazy val configLayer = Config.fromMap(Map(
     "APPLICATION_NAME" -> "zio-kafka-streams",
     "BOOTSTRAP_SERVERS" -> "localhost:9092",
     "SCHEMA_REGISTRY_URL" -> "http://localhost:8081"
   ), KafkaStreamsConfig.config)
 
-  private[this] lazy val loggingLayer =
-    Logging.console((_, logEntry) =>
-      logEntry
-    )
+  private[this] final lazy val loggingLayer =
+    Logging.console((_, logEntry) => logEntry)
 
-  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
-    program
-      .provideLayer(configLayer ++ loggingLayer)
-      .foldM(error => zio.console.putStrLn(s"ERROR: $error") *> UIO.succeed(1), _ => UIO.succeed(0))
-
-  final lazy val program: ZIO[Config[KafkaStreamsConfig] with Logging, Nothing, Unit] =
+  final lazy val program: ZIO[AppEnv, Nothing, Unit] =
     for {
       kafkaStreamsConfig <- config[KafkaStreamsConfig]
       _ <- log(kafkaStreamsConfig.applicationName)
     } yield ()
+
+  /*
+   * sbt "zio-kafka-streams/runMain com.kafka.demo.ZioKafkaStreamsApp"
+   */
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
+    program
+      .provideLayer(configLayer ++ loggingLayer)
+      .foldM(error => zio.console.putStrLn(s"ERROR: $error") *> UIO.succeed(1), _ => UIO.succeed(0))
 }
